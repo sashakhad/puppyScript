4c6bf610fed7720ef92e3f02fc36caa3b635fc30
Sasha Bayan <SashaBayan@gmail.com>
2015-04-24 12:48:22 -0700

    (fix) runs full-screen only once, pictures open smoothly and in order

--- who_let_the_dogs_out.sh
+++ who_let_the_dogs_out.sh
@@ -11,13 +11,24 @@
 #This limitation can be fixed by changing the for loop to look for a specifig Regular Expression
 #I may implement this feature in the future
 
+#create a flag so that a the full screen operation happens only once
+run=true
+
 for i in {1..18}
 do
+
   #open the image file
    open -a Preview ~/Documents/Private/unimportant/strange/how/odd/why/are/you/still/looking/here/$i.jpg; 
-   #stop for .3 seconds 
+  #pause for 1.5 second
+  sleep 1.5;
+
+  #full screen the picture after it has been opened
+  #this operation should only happen once; hence the $run flag
+  if [ $run = true ]; then
+    echo expression evaluated as true
+    /usr/bin/osascript -e 'tell application "Preview"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell";
    sleep .3;
-   #full screen the picture after it has been opened
-   /usr/bin/osascript -e 'tell application "Preview"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell";
-   sleep .3;
+   run=false
+  fi
+
 done
